0dc51cf7eb973ab88658f3bf9a8af7d65a36245e
isiah-a <isiaha478@gmail.com>
2025-02-27 16:34:53 -0500

    done with jshell and javaSmall

---
+++ SumNumbers.java
@@ -0,0 +1,239 @@
+ while (i <= 10) { System.out.println(i); i++;}
+printNumbers();
+printNumbers(5);
+printNumbers(10);
+printNumbers(20);
+sumNumbersTo10();
+ while (i <= 10) { System.out.println(i); i++;}
+printNumbers();
+printNumbers(5);
+printNumbers(10);
+printNumbers(20);
+sumNumbers(5);
+sumNumbers(11);
+sumNumbers(23);
+sumNumbers(5683);
+sumNumbers(106839);
+sumNumbers(5);
+sumNumbers(10);
+sumNumbers(20);
+sumNumbers(106839);
+sumNumbersTo10();
+sumNumbersTo10();
+ while (i <= 10) { System.out.println(i); i++;}
+printNumbers();
+printNumbers(5);
+printNumbers(10);
+printNumbers(20);
+sumNumbers(5);
+sumNumbers(11);
+sumNumbers(23);
+sumNumbers(5683);
+sumNumbers(106839);
+sumNumbers(5);
+sumNumbers(10);
+sumNumbers(20);
+sumNumbers(106839);
+sumNumbersTo10();
+sumNumbersTo10();
+ while (i <= 10) { System.out.println(i); i++;}
+printNumbers();
+printNumbers(5);
+printNumbers(10);
+printNumbers(20);
+sumNumbers(5);
+sumNumbers(11);
+sumNumbers(23);
+sumNumbers(5683);
+sumNumbers(106839);
+sumNumbers(5);
+sumNumbers(10);
+sumNumbers(20);
+sumNumbers(106839);
+sumNumbersTo10();
+sumNumbersTo10();
+ while (i <= 10) { System.out.println(i); i++;}
+printNumbers();
+printNumbers(5);
+printNumbers(10);
+printNumbers(20);
+sumNumbers(5);
+sumNumbers(11);
+sumNumbers(23);
+sumNumbers(5683);
+sumNumbers(106839);
+sumNumbers(5);
+sumNumbers(10);
+sumNumbers(20);
+sumNumbers(106839);
+sumNumbersTo10();
+sumNumbersTo10();
+ while (i <= 10) { System.out.println(i); i++;}
+printNumbers();
+printNumbers(5);
+printNumbers(10);
+printNumbers(20);
+sumNumbers(5);
+sumNumbers(11);
+sumNumbers(23);
+sumNumbers(5683);
+sumNumbers(106839);
+sumNumbers(5);
+sumNumbers(10);
+sumNumbers(20);
+sumNumbers(106839);
+sumNumbersTo10();
+sumNumbersTo10();
+ while (i <= 10) { System.out.println(i); i++;}
+printNumbers();
+printNumbers(5);
+printNumbers(10);
+printNumbers(20);
+sumNumbers(5);
+sumNumbers(11);
+sumNumbers(23);
+sumNumbers(5683);
+sumNumbers(106839);
+sumNumbers(5);
+sumNumbers(10);
+sumNumbers(20);
+sumNumbers(106839);
+sumNumbersTo10();
+sumNumbersTo10();
+ while (i <= 10) { System.out.println(i); i++;}
+printNumbers();
+printNumbers(5);
+printNumbers(10);
+printNumbers(20);
+sumNumbersTo10();
+ while (i <= 10) { System.out.println(i); i++;}
+printNumbers();
+printNumbers(5);
+printNumbers(10);
+printNumbers(20);
+sumNumbers(5);
+sumNumbers(11);
+sumNumbers(23);
+sumNumbers(5683);
+sumNumbers(106839);
+sumNumbers(5);
+sumNumbers(10);
+sumNumbers(20);
+sumNumbers(106839);
+sumNumbersTo10();
+sumNumbersTo10();
+ while (i <= 10) { System.out.println(i); i++;}
+printNumbers();
+printNumbers(5);
+printNumbers(10);
+printNumbers(20);
+sumNumbers(5);
+sumNumbers(11);
+sumNumbers(23);
+sumNumbers(5683);
+sumNumbers(106839);
+sumNumbers(5);
+sumNumbers(10);
+sumNumbers(20);
+sumNumbers(106839);
+sumNumbersTo10();
+sumNumbersTo10();
+ while (i <= 10) { System.out.println(i); i++;}
+printNumbers();
+printNumbers(5);
+printNumbers(10);
+printNumbers(20);
+sumNumbers(5);
+sumNumbers(11);
+sumNumbers(23);
+sumNumbers(5683);
+sumNumbers(106839);
+sumNumbers(5);
+sumNumbers(10);
+sumNumbers(20);
+sumNumbers(106839);
+sumNumbersTo10();
+sumNumbersTo10();
+ while (i <= 10) { System.out.println(i); i++;}
+printNumbers();
+printNumbers(5);
+printNumbers(10);
+printNumbers(20);
+sumNumbers(5);
+sumNumbers(11);
+sumNumbers(23);
+sumNumbers(5683);
+sumNumbers(106839);
+sumNumbers(5);
+sumNumbers(10);
+sumNumbers(20);
+sumNumbers(106839);
+sumNumbersTo10();
+sumNumbersTo10();
+ while (i <= 10) { System.out.println(i); i++;}
+printNumbers();
+printNumbers(5);
+printNumbers(10);
+printNumbers(20);
+sumNumbers(5);
+sumNumbers(11);
+sumNumbers(23);
+sumNumbers(5683);
+sumNumbers(106839);
+sumNumbers(5);
+sumNumbers(10);
+sumNumbers(20);
+sumNumbers(106839);
+sumNumbersTo10();
+sumNumbersTo10();
+ while (i <= 10) { System.out.println(i); i++;}
+void printNumbers() { int i = 1; while (i <= 10) { System.out.println(i); i++; }}
+printNumbers();
+void printNumbers(int n) { int i = 1; while (i <= n) { System.out.println(i); i++;}}
+printNumbers(5);
+printNumbers(10);
+printNumbers(20);
+sumNumbers(5);
+sumNumbers(11);
+sumNumbers(23);
+sumNumbers(5683);
+sumNumbers(106839);
+sumNumbers(5);
+sumNumbers(10);
+sumNumbers(20);
+sumNumbers(106839);
+sumNumbersTo10();
+sumNumbersTo10();
+int sumNumbers (int n) { int sum = 0; for (int i = 1; i <= n; i++){ sum = sum + i;} return sum;}
+ int sumNumbersTo10() { return sumNumbers(10);}
+int sumNumbersTo13() { return sumNumbers(13); }
+sumNumbersTo13();
+int addTowNumber(int x, int y) { return x + y;}
+int addTwoNumber(int x, int y) { return x + y;}
+addTwoNumber(5, 10);
+addTwoNumber(5, 10);
+int divideTwoNumbers(int x, int y) {return x / y;}
+int multiplyTwoNumbers(int x, int y) {return x * y;}
+int subtractTwoNumbers(int x, int y) {return x - y;}
+isEven(5);
+isEven(4);
+isEven(7);
+isEven(9);
+isEven(2);
+isEven(2);
+isEven(9);
+isOdd(3);
+int i = 1;
+ while (i <= 10) { System.out.println(i + " % 2 = " + i % 2); i++;}
+isOdd(9);
+isOdd(4);
+boolean isEven(int x) { if (x % 2 == 0) {return true;} else {return false;}}
+isEven(0);
+isEven(97);
+boolean isOdd(int x) { if (x % 2 != 0) {return true;} else {return false;}}
+isOdd(9);
+isOdd(78);
+isOdd(2);
+printOddEven();
+void printOddEven() { int i = 1; while (i <= 10) { if (isEven(i)) { System.out.println(i + " is even");} else {System.out.println(i + " is odd");}i++;}}
+printOddEven();
---
+++ helloWorld.java
@@ -0,0 +1,11 @@
+System.out.println("hello, World!");
+int x = 5;
+x
+x + 5;
+x
+x = x + 10
+x
+x
+x
+println(String s) { System.out.println(s); }
+println("your name here!");
